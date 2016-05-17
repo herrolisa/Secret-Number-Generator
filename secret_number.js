66e8327bb0f8d2c0a505de5c4047f7d74ae48beb
Lisa <misslisazhou@gmail.com>
2016-05-16 23:24:58 -1000

    complete assignment

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = (function() {
+  // var secretNumberGenerator will be assigned to this file/iife and will return this following function
+
+  return function() {
+    // this return function is assigned to secretNumber variable
+    // when invoked, secretNumberGenerator returns a random number
+    var newNum = (Math.random() * 10000) + 1;
+    // and also returns a function to generate that number
+    return function(){
+      return newNum;
+    };
+  };
+})();
